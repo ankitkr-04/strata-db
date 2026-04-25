9b93c6844767a7d9ed274c75bdd6a92c0619cdd6
Ankit Kumar <ak0182274@gmail.com>
2026-04-25 10:42:00 +0530

    feat(hardware): add function to detect optimal block size using ioctl

--- include/stratadb/utils/hardware.hpp
+++ include/stratadb/utils/hardware.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 #include <cstddef>
-#include <new>
+#include <linux/fs.h>
+#include <sys/ioctl.h>
 
 namespace stratadb::utils {
 
 // 64 bytes is the common L1 cache-line size on x86_64 and ARM64.
 inline constexpr std::size_t CACHE_LINE_SIZE = 64;
 
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+inline auto detect_optimal_block_size(int fd, size_t fallback_size) noexcept -> std::size_t {
+    unsigned int physical_block_size = 0;
+
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg,hicpp-vararg)
+    if (ioctl(fd, BLKPBSZGET, &physical_block_size) == 0) {
+        return physical_block_size;
+    }
+
+    return fallback_size;
+};
 } // namespace stratadb::utils
