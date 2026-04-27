be01dbb0d8f6f1456a42c28393ab783f12819cf5
Ankit Kumar <ak0182274@gmail.com>
2026-04-27 10:22:55 +0530

    fix(bytes): remove TiB constant and related operator to prevent overflow issues

--- include/stratadb/utils/bytes.hpp
+++ include/stratadb/utils/bytes.hpp
@@ -7,9 +7,10 @@ namespace stratadb::utils::bytes {
 inline constexpr std::size_t KiB = 1024ULL;
 inline constexpr std::size_t MiB = 1024ULL * KiB;
 inline constexpr std::size_t GiB = 1024ULL * MiB;
-inline constexpr std::size_t TiB = 1024ULL * GiB;
 
 namespace literals {
+// Terabytes can cause overflow in 32-bit systems, so we intentionally omit them to prevent misuse. and also it is not
+// currently needed in our use cases.
 consteval auto operator""_KiB(unsigned long long value) -> std::size_t {
     return static_cast<std::size_t>(value) * KiB;
 }
@@ -22,9 +23,6 @@ consteval auto operator""_GiB(unsigned long long value) -> std::size_t {
     return static_cast<std::size_t>(value) * GiB;
 }
 
-consteval auto operator""_TiB(unsigned long long value) -> std::size_t {
-    return static_cast<std::size_t>(value) * TiB;
-}
 } // namespace literals
 
 } // namespace stratadb::utils::bytes
