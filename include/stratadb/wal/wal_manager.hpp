fc7d7be303b79cf1e26eedaf54389f01335c34cf
Ankit Kumar <ak0182274@gmail.com>
2026-04-28 16:34:24 +0530

    add(wal_manager): allow WalManager constructor to include IO engine and file descriptor

--- include/stratadb/wal/wal_manager.hpp
+++ include/stratadb/wal/wal_manager.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "stratadb/config/wal_config.hpp"
+#include "stratadb/io/io_concept.hpp"
 #include "stratadb/memory/arena.hpp"
 #include "stratadb/memory/epoch_manager.hpp"
 #include "stratadb/wal/wal_staging.hpp"
 
 #include <cstdint>
 #include <span>
+#include <stop_token>
+#include <thread>
 #include <variant>
 
 namespace stratadb::wal {
@@ -18,9 +21,14 @@ using StagingVariant = std::variant<WalStaging<SectorSize::LegacyHDD>,
 
                                     >;
 
+template <io::IsIOEngine Engine>
 class WalManager {
   public:
-    WalManager(const config::WalConfig& config, memory::EpochManager& epoch_manager, memory::Arena& staging_arena);
+    WalManager(const config::WalConfig& config,
+               memory::EpochManager& epoch_manager,
+               memory::Arena& staging_arena,
+               Engine io_engine,
+               int wal_fd);
 
     [[nodiscard]] auto stage_write(std::uint64_t sequence_id,
                                    std::span<const std::byte> key,
@@ -33,10 +41,16 @@ class WalManager {
     void flush_pipeline() noexcept;
 
   private:
+    void flusher_loop(std::stop_token stop_token) noexcept;
+
+  private:
     StagingVariant staging_;
     config::WalConfig config_;
 
-    // TODO: add os-specific disk file descriptor and management
+    Engine io_engine_;
+    int wal_fd_{-1};
+
+    std::jthread flusher_thread_;
 };
 
 } // namespace stratadb::wal
