b5340f44d17a8e6826384b387dfbcce41025e15a
Ankit Kumar <ak0182274@gmail.com>
2026-05-23 09:41:40 +0530

    fix(bytes): correct size constants to use 1024UZ for consistency

--- include/stratadb/utils/bytes.hpp
+++ include/stratadb/utils/bytes.hpp
@@ -4,8 +4,8 @@
 
 namespace stratadb::utils::bytes {
 
-inline constexpr std::size_t KiB = 1024ULL;
-inline constexpr std::size_t MiB = 1024ULL * KiB;
-inline constexpr std::size_t GiB = 1024ULL * MiB;
+inline constexpr std::size_t KiB = 1024UZ;
+inline constexpr std::size_t MiB = 1024UZ * KiB;
+inline constexpr std::size_t GiB = 1024UZ * MiB;
 
 } // namespace stratadb::utils::bytes
